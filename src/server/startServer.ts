9e867bb83f131b9758e2fbf566675fa801d5dc83
Albert Colubi <albertmc89@gmail.com>
2023-10-05 18:51:37 +0200

    Add startServer function and start express in index.ts

---
+++ src/server/startServer.ts
@@ -0,0 +1,14 @@
+import "dotenv/config";
+
+import debugCreator from "debug";
+import app from "./index.js";
+
+const debug = debugCreator("properties-api:start");
+
+const startServer = (port: number | string) => {
+  app.listen(port, () => {
+    debug(`Listening on http://localhost:${port}`);
+  });
+};
+
+export default startServer;

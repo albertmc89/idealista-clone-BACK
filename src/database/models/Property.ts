6a11d0dfa4b1b4d62fa1fe342dd3c6ae1492cfec
Albert Colubi <albertmc89@gmail.com>
2023-10-05 20:48:27 +0200

    Create property model schema and types

---
+++ src/database/models/Property.ts
@@ -0,0 +1,74 @@
+import { Schema, model } from "mongoose";
+import { type PropertyStructure } from "../../types";
+
+const playerSchema = new Schema<PropertyStructure>({
+  address: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  rooms: {
+    type: Number,
+    required: true,
+  },
+  meters: {
+    type: Number,
+    required: true,
+  },
+  bathrooms: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  parking: {
+    type: Boolean,
+    required: true,
+  },
+  elevator: {
+    type: Boolean,
+    required: true,
+  },
+  heating: {
+    type: Boolean,
+    required: true,
+  },
+  aircon: {
+    type: Boolean,
+    required: true,
+  },
+  consumption: {
+    type: Number,
+    required: true,
+  },
+  emissions: {
+    type: Number,
+    required: true,
+  },
+  level: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: String,
+    ref: "User",
+    required: true,
+  },
+});
+
+const Player = model("Player", playerSchema, "players");
+
+export default Player;
--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,23 @@ export interface UserStructure {
   name: string;
   authId: string;
 }
+
+export interface PropertyStructure {
+  id: string;
+  address: string;
+  price: number;
+  rooms: number;
+  meters: number;
+  year: number;
+  bathrooms: number;
+  aircon: boolean;
+  consumption: number;
+  elevator: boolean;
+  parking: boolean;
+  heating: boolean;
+  emissions: number;
+  level: string;
+  description: string;
+  image: string;
+  user: string;
+}
